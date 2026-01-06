8d07306f6b23ac204825908c10cc3ffb44af6a11
Khloe Islam <khloei2@nycstudents.net>
2026-01-06 18:59:56 +0000

    dsgfd

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,24 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+
+function fruitcake1(){
+    document.getElementById("highlight").innerHTML="Fruit Cake #1";
+}
+
+function fairybread1(){
+    document.getElementById("highlight").innerHTML="Fairy Bread #1";
+}
+
+function fruitcake2(){
+    document.getElementById("highlight").innerHTML="Fruit Cake #2";
+}
+
+function fairybread2(){
+    document.getElementById("highlight").innerHTML="Fairy Bread #2";
+}
+function fruitcake3(){
+    document.getElementById("highlight").innerHTML="Fruit Cake #3";
+}
+
+
+
+
