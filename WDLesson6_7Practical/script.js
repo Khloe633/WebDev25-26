1b139846f1d7a2d2f5e426f00f992eaca064758f
Khloe Islam <khloei2@nycstudents.net>
2026-04-30 18:08:07 +0000

    r6yry

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -11,14 +11,14 @@ async function init(){
   for(let i = 0; i < data.length; i+=1){
     let crash = data[i];
     build += `<div class="fitted card">
-                 <h3>ID: ${crash.collision_id}</h3>
+                 <h2>ID: ${crash.collision_id}</h2>
                  <hr>
-                <h5>Date: ${crash.crash_date}</h5>
-              <p>${crash.on_street_name}, ${crash.borough}</p>
+                <h4>Date: ${crash.crash_date}</h4>
+              <p>Location: ${crash.on_street_name}, ${crash.borough}</p>
               <p>People injured:${crash.number_of_persons_injured}</p>
                 <p>People killed:${crash.number_of_persons_killed}</p>
+                <h5> Vehicle Type: ${crash.vehicle_type_code1}</h5>
                 <h5>Contributing Factor: ${crash.contributing_factor_vehicle_1}</h5>
-                <p> Vehicle Type: ${crash.vehicle_type_code1}</p>
               </div>`    
   }
   output.innerHTML = build;
@@ -34,49 +34,47 @@ function filterByBorough(){
   let ct = 0;
 
   for(let i = 0; i < data.length; i+=1){
-    let complaint = data[i];
-    if(complaint.borough == borough){
-      build += `<div class="fitted card">
-                    <h3>${complaint.complaint_type}</h3>
-                    <hr>
-                    <p>${complaint.borough}</p>
-                    <p>${complaint.incident_zip}</p>
-                    <p>${complaint.descriptor}</p>
-                    <hr>
-                    <p>${complaint.created_date}</p>
-                    <hr>
-                    <p>${complaint.agency}</p>
+    let crash = data[i];
+    if(crash.borough == borough){
+       build += `<div class="fitted card">
+                 <h3>ID: ${crash.collision_id}</h3>
+                 <hr>
+                <h5>Date: ${crash.crash_date}</h5>
+              <p>${crash.on_street_name}, ${crash.borough}</p>
+              <p>People injured:${crash.number_of_persons_injured}</p>
+                <p>People killed:${crash.number_of_persons_killed}</p>
+                <h5> Vehicle Type: ${crash.vehicle_type_code1}</h5>
+                <h5>Contributing Factor: ${crash.contributing_factor_vehicle_1}</h5>
                 </div>`;
       ct += 1;
     }
   }
-  result.innerHTML = `${ct} Results found.`
+  result.innerHTML = `${ct} Results found.`;
   output.innerHTML = build;
 }
 
 // Challenge 2: Create an event handler (function) to filter the 311 Service Request by zip code.
 
-function filterByZip(){
+function filterByFactor(){
   let output = document.getElementById("output");
-  let zip = document.getElementById("zip").value;
+  let factor = document.getElementById("factor").value;
   let result = document.getElementById("result");
   
   let build = "";
   let ct = 0;
 
   for(let i = 0; i < data.length; i+=1){
-    let complaint = data[i];
-    if(complaint.incident_zip == zip){
+    let crash = data[i];
+    if(crash.contributing_factor_vehicle_1 == factor){
       build += `<div class="fitted card">
-                    <h3>${complaint.complaint_type}</h3>
-                    <hr>
-                    <p>${complaint.borough}</p>
-                    <p>${complaint.incident_zip}</p>
-                    <p>${complaint.descriptor}</p>
-                    <hr>
-                    <p>${complaint.created_date}</p>
-                    <hr>
-                    <p>${complaint.agency}</p>
+                 <h3>ID: ${crash.collision_id}</h3>
+                 <hr>
+                <h5>Date: ${crash.crash_date}</h5>
+              <p>${crash.on_street_name}, ${crash.borough}</p>
+              <p>People injured:${crash.number_of_persons_injured}</p>
+                <p>People killed:${crash.number_of_persons_killed}</p>
+                <h5> Vehicle Type: ${crash.vehicle_type_code1}</h5>
+                <h5>Contributing Factor: ${crash.contributing_factor_vehicle_1}</h5>
                 </div>`;
       ct += 1;
     }
@@ -88,27 +86,26 @@ function filterByZip(){
 
 // Challenge 4: Create an event handler (function) to filter the 311 Service Request by complaint type.
 
-function filterByComplaint(){
+function filterByvehicle(){
   let output = document.getElementById("output");
-  let complainttype = document.getElementById("complainttype").value;
+  let vehicle = document.getElementById("vehicle").value;
   let result = document.getElementById("result");
   
   let build = "";
   let ct = 0;
 
   for(let i = 0; i < data.length; i+=1){
-    let complaint = data[i];
-    if(complaint.complaint_type == complainttype){
-      build += `<div class="fitted card">
-                    <h3>${complaint.complaint_type}</h3>
-                    <hr>
-                    <p>${complaint.borough}</p>
-                    <p>${complaint.incident_zip}</p>
-                    <p>${complaint.descriptor}</p>
-                    <hr>
-                    <p>${complaint.created_date}</p>
-                    <hr>
-                    <p>${complaint.agency}</p>
+    let crash = data[i];
+    if(crash.vehicle_type_code1 == vehicle){
+    build += `<div class="fitted card">
+                 <h3>ID: ${crash.collision_id}</h3>
+                 <hr>
+                <h5>Date: ${crash.crash_date}</h5>
+              <p>${crash.on_street_name}, ${crash.borough}</p>
+              <p>People injured:${crash.number_of_persons_injured}</p>
+                <p>People killed:${crash.number_of_persons_killed}</p>
+                <h5> Vehicle Type: ${crash.vehicle_type_code1}</h5>
+                <h5>Contributing Factor: ${crash.contributing_factor_vehicle_1}</h5>
                 </div>`;
       ct += 1;
     }
