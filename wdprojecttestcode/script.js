b80e0e04c1f558e34db6608b7b7a47341d2a5aec
Khloe Islam <khloei2@nycstudents.net>
2026-05-23 00:28:14 +0000

    jbj

--- wdprojecttestcode/script.js
+++ wdprojecttestcode/script.js
@@ -15,12 +15,43 @@ async function init(){
                  <br>${farm.marketname}</h3>
                  <hr>
               <p>Location: ${farm.streetaddress}, ${farm.borough}</p>
-              <br>
-              <p>Community District: ${farm.community_district}</p>
+              <p>District: ${farm.community_district}</p>
               <p>Operating hours:${farm.daysoperation}, ${farm.hoursoperations}</p>
               <p>Open year round?:${farm.open_year_round}</p>
               <p>Accepts EBT?: ${farm.accepts_ebt}</p>
               </div>`    
   }
   output.innerHTML = build;
+}
+
+
+function filterbyboroandebt(){
+  let boro= get("boro").value;
+  let ebt= get("ebt").value;
+  let build ="";
+
+
+   for(let i=0; i<data.length; i++){
+    let farm=data[i];
+    if(farm.borough == boro && farm.accepts_ebt == ebt){
+      build+= card(farm);
+    }
+  }
+  output.innerHTML=build;
+}
+
+
+function filterbyoperatingdayandhour(){
+  let day= get("day").value;
+  let hours= get("hours").value;
+  let build ="";
+
+
+   for(let i=0; i<data.length; i++){
+    let farm=data[i];
+    if(farm.hoursoperations == hours && farm.daysoperation == day){
+      build+= card(farm);
+    }
+  }
+  output.innerHTML=build;
 }
